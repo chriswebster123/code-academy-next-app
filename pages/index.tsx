852edb7f15ccf7e4dc67ae07bde8a8efba2375b0
chriswebster123 <chris_webster1@msn.com>
2021-09-09 12:38:53 +0000

    website updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,19 @@ import Head from "next/head";
 import Image from "next/image";
 import Card from "../components/card";
 import Product from "../components/product";
+import { useEffect, useState } from "react";
 
 const products = ["T-Shirt", "Jacket", "Hat"];
 
 export default function Home() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await (await fetch(`api/products`)).json();
+      setProducts(res);
+    };
+    fetchProducts();
+  }, []);
   return (
     <div className="min-h-screen flex flex-col justify-center items-center">
       <main className="py-20 flex flex-col flex-1 justify-center items-center">
@@ -18,15 +27,8 @@ export default function Home() {
         </p>
 
         <div className="mt-10 flex flex-wrap flex-col sm:flex-row w-full sm:max-w-3xl justify-center items-center">
-          {products.map((p, i) => (
-            <Product
-              id={i + 1}
-              key={p}
-              name={p}
-              category={1}
-              cost={10}
-              image={i % 4}
-            />
+          {products.map((p) => (
+            <Product key={p.id} {...p} />
           ))}
         </div>
       </main>
