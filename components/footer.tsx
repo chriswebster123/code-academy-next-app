074ce7f10544ac4fc249f75807d6830c0e5da5e9
chriswebster123 <chris_webster1@msn.com>
2021-09-24 16:28:40 +0000

    Updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,37 +1,20 @@
 import React from "react";
 
-function Footer() {
+const Footer: React.FC = () => {
   return (
     <div className="main-footer">
       <div className="container">
         <div className="row">
-          {/* Column1 */}
-          <div className="column">
-            <h4>SIDETRADE</h4>
-            <h1 className="list-unstyled">
-              <li>Contact Us</li>
-              <li>About Us</li>
-              <li>Terms of Use</li>
-            </h1>
-          </div>
-          {/* Column2 */}
-          <div className="column">
-            <h4>SIDETRADE</h4>
-            <h1 className="list-unstyled">
-              <li>About Us</li>
-            </h1>
-          </div>
-          <hr />
-          <div className="row">
-            <p className="col-sm">
-              &copy;{new Date().getFullYear()} SIDETRADE | All rights reserved |
-              Terms Of Service | Privacy
-            </p>
-          </div>
+          <span className=" ml-4 flex items-center justify-center">
+            <h2>SIDETRADE</h2>
+            <p>Contact Us</p>
+            <p>About Us</p>
+            <p>Terms of Use</p>
+          </span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Footer;
